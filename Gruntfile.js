3775a9c182c515d19204d374796663d8798bd5b0
Adrian <adrian.sanchez@go-sharp.com>
2020-08-16 18:08:04 -0500

    Se configuro grunt

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		connect: {
+			server: {
+				options: {
+					port: 9000,
+					base: './',
+				},
+			},
+		},
+		watch: {
+			project: {
+				files: [
+					'./assets/services/*.js',
+					'./assets/directives/*.js',
+					'./assets/controllers/*.js',
+					'./assets/js/*.js',
+					'./*.html',
+					'./assets/directives/partials/*.html',
+					'./assets/css/*.css',
+				],
+				options: {
+					livereload: true,
+				},
+			},
+		},
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['connect', 'watch']);
+};
